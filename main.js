58a2feda0864975bb9292bdbd83216fb4fd69a5f
dushaboev <dushaboyevdoston0@email.com>
2023-08-23 00:27:37 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,119 @@
+// 1-masala
+
+// let k = 20;
+// let n = 6;
+
+// for (let i = 1; i <= n; i++){
+//     console.log(k);
+// }
+
+// 2-masala
+
+// let a = 1;
+// let b = 25;
+
+// for (let i = a; i <= b; i++){
+//     console.log(i);
+// }
+
+// 3-masala 
+
+// let a = 10; 
+// let b = 2;
+// for (let i = a - 1; i > b; i --) {
+//     console.log(i);
+// }
+
+// 4-masala
+
+// let a = 10000;
+
+// for (let i = 1; i <= 10; i ++){
+//     console.log(`${k} sum`);
+//     k += 10000
+// } 
+
+// 5-masala
+
+// let a = 1000;
+
+// for (let i = 1; i <= 10; i ++){
+//         console.log(`${k} sum`);
+//         k += 1000
+//     } 
+
+// 6-masala
+
+// let a = 12000
+
+// for (let i = 1; i <= 5; i ++){
+//     console.log(`${a} sum`);
+//     a += 2000
+// } 
+
+// 7-masala
+
+// let res = 0; 
+// let a = 4;
+// let b = 14;
+
+// for (let i = a; i <= b; i ++){
+//      res += i
+// }
+// console.log(res ** 2);  
+
+// 8-masala
+
+// let res = 1; 
+// let a = 4;
+// let b = 8;
+
+// for (let i = a; i <= b; i ++){
+//      res *= i
+// }
+// console.log(res);  
+
+// 9-masala
+
+// let Sum = 0
+
+// function sum(a,b){
+//     for(let i = a; i<b;i++){
+//         let pow = Math.pow(2,i)
+//         Sum+=pow   
+//     }
+//     console.log(Sum)
+// }
+// sum(1,4)
+
+// 10-masala
+
+// 11-masala
+
+// let n = 20
+
+// for (let i = 1; i < n; i++){
+//     if (i % 2 != 0) console.log(i);
+// } 
+
+// 12-masala
+
+// let n =15
+
+// for (let i = 1; i < n; i++){
+//     if (i % 2 != 1) console.log(i);
+// }
+
+// 13-masala
+
+// 14-masala
+
+// 15-masala
+
+// let n = 17;
+
+// for (let i = n; i > 0; i--){
+//     console.log(i);
+// }
+
+// 16-masala
